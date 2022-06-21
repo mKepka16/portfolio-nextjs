fb0a633dae01426560ff9e8740bd7b207fc52928
Michał Kępka <michal.kepka16@gmail.com>
2022-06-21 15:45:56 +0200

    setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
+import axios from 'axios';
+
+axios.defaults.headers.common[
+  'Authorization'
+] = `bearer ${process.env.NEXT_PUBLIC_STRAPI_TOKEN}`;
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_STRAPI_URL;
+console.log(process.env.NEXT_PUBLIC_STRAPI_TOKEN);
+console.log(process.env.NEXT_PUBLIC_STRAPI_URL);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
